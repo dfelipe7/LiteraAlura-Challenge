60654723c9b5a90e18e2bb2b7bdcc617f8743f77
dfelipe7 <duvanfelipe777@outlook.com>
2025-07-25 22:17:47 -0500

    dtos

---
+++ src/main/java/co/edu/unicauca/literaAlura/LiteraAluraApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.literaAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraAluraApplication.class, args);
+	}
+
+}
